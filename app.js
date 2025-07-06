0b039a2b724206384025f9458db6e05ae1983140
Ashmit Shrivastava <2205889@kiit.ac.in>
2025-07-06 18:32:58 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,144 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOveride = require("method-override");
+const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(methodOveride("_method"));
+app.engine("ejs", ejsMate);
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/", (req, res) => {
+  res.send("HYY");
+});
+
+//middleware for validating listing schema
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, error);
+  } else {
+    next();
+  }
+};
+
+//index route
+app.get(
+  "/listing",
+  wrapAsync(async (req, res) => {
+    const allListing = await Listing.find({});
+    res.render("listings/index.ejs", { allListing });
+  })
+);
+
+//new route
+app.get("/listing/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+//show route
+app.get(
+  "/listing/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+  })
+);
+
+//create route
+app.post(
+  "/listing",
+  validateListing,
+  wrapAsync(async (req, res, next) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listing");
+  })
+);
+
+//edit route
+app.get(
+  "/listing/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+  })
+);
+
+//update route
+app.put(
+  "/listing/:id",
+  validateListing,
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listing/${id}`);
+  })
+);
+
+//delete route
+app.delete(
+  "/listing/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listing");
+  })
+);
+
+// app.get("/testListing", async(req,res)=>{
+//     let sampleListing = new Listing({
+//         title : "My New Villa",
+//         description : "By the Beach",
+//         price : 1200,
+//         location : "Calangute, Goa",
+//         country : "India",
+//     });
+
+//     await sampleListing.save();
+//     console.log("sample was saved");
+//     res.send("successful testing");
+// });
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "Page not Found"));
+});
+
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something went wrong!" } = err;
+  res.status(statusCode).render("error.ejs", { message });
+  // res.status(statusCode).send(message);
+});
+
+let a = "http://localhost:8080/listing";
+
+app.listen(8080, () => {
+  console.log(`server is listening : ${a}`);
+});
---
+++ schema.js
@@ -0,0 +1,13 @@
+const Joi = require("joi");
+const { join } = require("path");
+
+module.exports.listingSchema = Joi.object({
+  listing: Joi.object({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    location: Joi.string().required(),
+    country: Joi.string().required(),
+    price: Joi.number().required().min(0),
+    "image.url": Joi.string().required(),
+  }).required(),
+});
